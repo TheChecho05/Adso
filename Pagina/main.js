56d84b0cb33d6067ef3c3fbc245c3375328e1661
Checho05 <146319633+TheChecho05@users.noreply.github.com>
2024-02-17 23:46:32 -0500

    Add files via upload

---
+++ Pagina/main.js
@@ -0,0 +1,9 @@
+function llevarquien(){
+    location.href = "/Trabajos/quiensoy/quien.html"
+}
+function calendario(){
+    location.href = "/Trabajos/Calendario/calendario.html"
+}
+function factura(){
+    location.href = "/Trabajos/Factura/fact.html"
+}
